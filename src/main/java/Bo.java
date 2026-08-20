663a4c584c5b3f3d23075360a5e0ef7d1c02a581
currycurry11 <lhyjoshua04@gmail.com>
2026-08-20 10:06:51 +0800

    added ascii banner

--- src/main/java/Bo.java
+++ src/main/java/Bo.java
@@ -1,6 +1,13 @@
 public class Bo {
     public static void main(String[] args) {
-        String banner = "I am Bo, your personel assistant\n";
+        String banner = """
+                 ____
+                | __ )  ___
+                |  _ \\ / _ \\
+                | |_) | (_) |
+                |____/ \\___/
+                """;
+
         System.out.println(banner);
         System.out.println("Hello! I'm Bo.");
         System.out.println("What can I do for you?");
