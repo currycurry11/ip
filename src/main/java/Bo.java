1584c0c641fb24fb5e56790098f0debebae0ef38
currycurry11 <lhyjoshua04@gmail.com>
2026-08-20 10:05:38 +0800

    added greeting texts

--- src/main/java/Bo.java
+++ src/main/java/Bo.java
@@ -2,5 +2,9 @@ public class Bo {
     public static void main(String[] args) {
         String banner = "I am Bo, your personel assistant\n";
         System.out.println(banner);
+        System.out.println("Hello! I'm Bo.");
+        System.out.println("What can I do for you?");
+        System.out.println();
+        System.out.println("Bye. Hope to see you again soon!");
     }
 }
