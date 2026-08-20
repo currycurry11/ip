2463f2767320d0e9ab8870ba0fd06eea6149812d
currycurry11 <lhyjoshua04@gmail.com>
2026-08-20 11:20:22 +0800

    updated tracker to use an Arraylist and handle delete tasks

--- src/main/java/Tracker.java
+++ src/main/java/Tracker.java
@@ -1,18 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Stores and displays the tasks entered during one run of Bo.
  */
 public class Tracker {
-    private static final int MAX_TASKS = 100;
-
-    private final Task[] tasks;
-    private int taskCount;
+    private final List<Task> tasks;
 
     /**
-     * Creates an empty tracker that can store up to 100 tasks.
+     * Creates an empty tracker.
      */
     public Tracker() {
-        tasks = new Task[MAX_TASKS];
-        taskCount = 0;
+        tasks = new ArrayList<>();
     }
 
     /**
@@ -21,13 +20,10 @@ public class Tracker {
      * @param task the task object to store
      */
     public void addTask(Task task) {
-        if (taskCount < MAX_TASKS) {
-            tasks[taskCount] = task;
-            taskCount++;
-            System.out.println(" Got it. I've added this task:");
-            System.out.println("   " + task);
-            System.out.println(" Now you have " + taskCount + " tasks in the list.");
-        }
+        tasks.add(task);
+        System.out.println(" Got it. I've added this task:");
+        System.out.println("   " + task);
+        System.out.println(" Now you have " + tasks.size() + " tasks in the list.");
     }
 
     /**
@@ -35,8 +31,8 @@ public class Tracker {
      */
     public void printTasks() {
         System.out.println(" Here are the tasks in your list:");
-        for (int i = 0; i < taskCount; i++) {
-            System.out.println(" " + (i + 1) + "." + tasks[i]);
+        for (int i = 0; i < tasks.size(); i++) {
+            System.out.println(" " + (i + 1) + "." + tasks.get(i));
         }
     }
 
@@ -47,7 +43,7 @@ public class Tracker {
      * @return true if the task number is valid
      */
     public boolean isValidTaskNumber(int taskNumber) {
-        return taskNumber >= 1 && taskNumber <= taskCount;
+        return taskNumber >= 1 && taskNumber <= tasks.size();
     }
 
     /**
@@ -56,7 +52,7 @@ public class Tracker {
      * @param taskNumber the task number displayed in the list
      */
     public void markTask(int taskNumber) {
-        Task task = tasks[taskNumber - 1];
+        Task task = tasks.get(taskNumber - 1);
         task.markAsDone();
         System.out.println(" Nice! I've marked this task as done:");
         System.out.println("   " + task);
@@ -68,9 +64,21 @@ public class Tracker {
      * @param taskNumber the task number displayed in the list
      */
     public void unmarkTask(int taskNumber) {
-        Task task = tasks[taskNumber - 1];
+        Task task = tasks.get(taskNumber - 1);
         task.markAsNotDone();
         System.out.println(" OK, I've marked this task as not done yet:");
         System.out.println("   " + task);
+    }
+
+    /**
+     * Removes a numbered task and displays its confirmation message.
+     *
+     * @param taskNumber the task number displayed in the list
+     */
+    public void deleteTask(int taskNumber) {
+        Task task = tasks.remove(taskNumber - 1);
+        System.out.println(" Noted. I've removed this task:");
+        System.out.println("   " + task);
+        System.out.println(" Now you have " + tasks.size() + " tasks in the list.");
     }
 }
